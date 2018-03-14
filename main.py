033b108edd90d4c3182b581eaed05a78bd79a48b
MingukKANG <30516904+MINGUKKANG@users.noreply.github.com>
2018-03-14 09:29:23 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import data
+import model
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_integer("z_d", 100, "Dimension of z")
+flags.DEFINE_float("learning_rate", 0.00004, "learning_rate")
+flags.DEFINE_integer("batch_size", 64, "batch_size")
+flags.DEFINE_integer("n_epoch", 100, "number of epoch")
+
+train_data,train_labels,validation_data,validation_labels,test_data,test_labels = data.prepare_MNIST_Data()
+train_data = data.preprocessing_data(train_data)
+sess = tf.Session()
+network = model.Gan(sess,[64,64,1],FLAGS.z_d, FLAGS.learning_rate, FLAGS.batch_size)
+sess.run(tf.initialize_all_variables())
+network.training(train_data,FLAGS.n_epoch)
+network.testing()
